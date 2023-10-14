2eb007bd615de6c406bb2debe7e7a0c83bb5785f
Muaz Munir <muazmuneer151m@gmail.com>
2023-10-14 15:58:46 +0500

    v11

--- resources/views/comments.blade.php
+++ resources/views/comments.blade.php
@@ -1,10 +1,15 @@
 @foreach ($comments as $comment)
     <div class="card mt-2">
-        <div class="card-header">
+        <div class="card-header justify-content-between d-flex">
+            <div>
             {{ $comment->user->name }}
+            </div>
+            <div>
+                {{ $comment->created_at }}
+            </div>
         </div>
         <div class="card-body">
-            {{ $comment->content }}
+            {!! $comment->content !!}
         </div>
     </div>
 @endforeach
